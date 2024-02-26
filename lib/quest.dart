46be99c77cc918bc0b3a895af24f472805b0e16e
li <160301247+Jahid-sj@users.noreply.github.com>
2024-02-26 15:28:51 +0530

    initial commit

--- lib/quest.dart
+++ lib/quest.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Quest extends StatefulWidget {
   const Quest({Key? key}) : super(key: key);
 
@@ -10,8 +11,40 @@ class _QuestState extends State<Quest> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black
-      );
-
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'flutter is an open source framework by google',
+              style: TextStyle(fontSize: 22, color: Colors.white),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.green),
+                  onPressed: () {},
+                  child: Text('True')),
+              height: 50,
+              width: 100,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.red),
+                  onPressed: () {},
+                  child: Text('False')),
+              height: 50,
+              width: 100,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
